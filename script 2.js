739e02df4194e0b2c44328976b524d6b0aa744c8
Monique Jelvard <moniquejelvardm@hotmail.com>
2024-07-25 19:16:41 +0200

    ændringer

--- script 2.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const burgerMenu = document.getElementById("burger-menu");
-  const nav = document.getElementById("nav");
-
-  burgerMenu.addEventListener("click", function () {
-    nav.classList.toggle("active");
-    burgerMenu.classList.toggle("toggle");
-  });
-});
